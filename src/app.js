cd72b90ba03e5c18d5e643f7c07fbdb4af02e434
Nuno Farinha <nadfarinha@gmail.com>
2019-02-27 13:28:15 +0000

    Add required classes to app entry point

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+const Countries = require('./models/countries.js');
+const CountryView = require('./views/country_view.js')
+
 document.addEventListener('DOMContentLoaded', () => {
+//DELETE
   console.log('JavaScript Loaded');
+  const countries = new Countries();
+  countries.getData();
+
+  const countriesContainer = document.querySelector('#country');
+  const countryView = new CountryView(countriesContainer);
+  countryView.bindEvents();
+
 });
